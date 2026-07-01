3a496d152fdc4f0b1cdfb4888482ae7a32a73198
Lev-Dobrianskyi <lionnametu@gmail.com>
2026-07-01 22:13:42 +0300

    Commit tutorial 2.1

--- Music App/MainMenuForm.Designer.cs
+++ Music App/MainMenuForm.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            label3 = new Label();
             label2 = new Label();
             SuspendLayout();
             // 
@@ -42,13 +41,6 @@
             label1.Size = new Size(749, 45);
             label1.TabIndex = 0;
             label1.Text = "Yooo so uhh just use this as the main menu thanks!";
-            // 
-            // label3
-            // 
-            label3.Location = new Point(0, 0);
-            label3.Name = "label3";
-            label3.Size = new Size(100, 23);
-            label3.TabIndex = 0;
             // 
             // label2
             // 
@@ -76,7 +68,6 @@
         #endregion
 
         private Label label1;
-        private Label label3;
         private Label label2;
     }
 }
