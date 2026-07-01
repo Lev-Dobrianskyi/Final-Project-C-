f054606b7b0262e36a9eef70db06d7deea1b2d04
Lev-Dobrianskyi <lionnametu@gmail.com>
2026-07-01 20:33:27 +0300

    Updated the main form

--- Music App/MainMenuForm.Designer.cs
+++ Music App/MainMenuForm.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainMenuForm";
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F);
+            label1.Location = new Point(25, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(749, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Yooo so uhh just use this as the main menu thanks!";
+            // 
+            // MainMenuForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Name = "MainMenuForm";
+            Text = "MainMenuForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
