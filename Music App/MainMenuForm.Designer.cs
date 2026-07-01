b744de99a7e4e4cca6de394b41e25d3a37931534
Lev-Dobrianskyi <lionnametu@gmail.com>
2026-07-01 22:11:38 +0300

    Commit tutorial 2

--- Music App/MainMenuForm.Designer.cs
+++ Music App/MainMenuForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             label1 = new Label();
             label3 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -49,11 +50,22 @@
             label3.Size = new Size(100, 23);
             label3.TabIndex = 0;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 24F);
+            label2.Location = new Point(246, 190);
+            label2.Name = "label2";
+            label2.Size = new Size(249, 45);
+            label2.TabIndex = 1;
+            label2.Text = "Work from dev2";
+            // 
             // MainMenuForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
             Controls.Add(label1);
             Name = "MainMenuForm";
             Text = "MainMenuForm";
@@ -65,5 +77,6 @@
 
         private Label label1;
         private Label label3;
+        private Label label2;
     }
 }
