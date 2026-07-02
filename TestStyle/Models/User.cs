4f6b90aa6840da1d3f399b6f17fd5f85b4b36a10
teacher <>
2026-07-02 18:13:46 +0300

    test commit

---
+++ TestStyle/Models/User.cs
@@ -0,0 +1,19 @@
+namespace UserModel;
+
+/// <summary>
+/// User class
+/// </summary>
+class User
+{
+    private string _id;
+    private string _name;
+
+    /// <summary>
+    /// get id.
+    /// </summary>
+    /// <returns>_id.</returns>
+    public string getId()
+    {
+        return _id;
+    }
+}
